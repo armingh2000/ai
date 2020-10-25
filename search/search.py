c6b270e879bf3d07ba72e1210ff76887cd09d362
armingh2000 <armingholampour71@gmail.com>
2020-10-25 17:34:54 -0400

    q4 passed

--- search/search.py
+++ search/search.py
@@ -169,55 +169,31 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    from game import Directions
-    n, s, w, e = Directions.NORTH, Directions.SOUTH, Directions.WEST, Directions.EAST
-
-    def getDirection(d):
-        if d == "West":
-            return w
-        elif d == "East":
-            return e
-        elif d == "North":
-            return n
-        elif d == "South":
-            return s
-
-        else:
-            return d
-
-
-    def findPath(parents, problem):
-        way = []
-        goalState = [m for m in parents.keys() if problem.isGoalState(m)][0]
-        currentState = goalState
-        while currentState != problem.getStartState():
-            way.append(getDirection(parents[currentState][1]))
-            currentState = parents[currentState][0]
-
-        way.reverse()
-        return way
 
     def localHeuristic(position):
         return heuristic(position, problem)
 
-
-    parents = dict()
+    fringe = util.PriorityQueue()
     visited = set()
-    toSee = util.PriorityQueueWithFunction(localHeuristic)
-    toSee.push(problem.getStartState())
+    currentState = problem.getStartState()
+    fringe.push((currentState, [], 0), 0)
 
-    while True:
-        state = toSee.pop()
-        if problem.isGoalState(state):
-            break
-        for succ in problem.getSuccessors(state):
+    while not fringe.isEmpty():
+        currentState, path, cost= fringe.pop()
+
+        if problem.isGoalState(currentState):
+            return path
+
+        if currentState in visited:
+            continue
+
+        for succ in problem.getSuccessors(currentState):
             if not succ[0] in visited:
-                toSee.push(succ[0])
-                parents[succ[0]] = (state, succ[1])
+                fringe.update((succ[0], path + [succ[1]], cost + succ[2]), localHeuristic(succ[0]) + succ[2] + cost)
 
-            visited.add(state)
+        visited.add(currentState)
 
-    return findPath(parents, problem)
+    return []
 
 
 
